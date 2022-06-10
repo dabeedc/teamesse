9a24e315cfd06d65b230044dc611be6b9f673e30
Scott <scott.m.king@outlook.com>
2022-06-09 19:49:01 -0700

    Test

--- pomodoro/src/App.js
+++ pomodoro/src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import { Clock } from "./clock/Clock";
 import { ColorSampler } from "./components/ColorSampler";
 import { Profile } from "./profile/Profile"
+
+// random comment
 function App() {
   return (
     <div className="App">
