75dbad7e63321e1595755d9f9652825c06227d2d
JamesJHPark <jamesjhp.park@gmail.com>
2022-06-09 18:25:55 -0700

    implement user profile page

--- pomodoro/src/App.js
+++ pomodoro/src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import { ColorSampler } from "./components/ColorSampler";
-
+import { Profile } from "./profile/Profile"
 function App() {
   return (
     <div className="App">
       <ColorSampler />
+      <Profile />
     </div>
   );
 }
