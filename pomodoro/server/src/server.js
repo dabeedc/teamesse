cad750fce0645c738424307936151bed83c43e7b
Scott <scott.m.king@outlook.com>
2022-06-18 18:00:37 -0700

    Comments

--- pomodoro/server/src/server.js
+++ pomodoro/server/src/server.js
@@ -4,10 +4,15 @@ const cors = require("cors");
 const PORT = 3001;
 
 const app = express();
+
+/** Middleware */
 app.use(express.json());
 app.use(cors());
+
+/** Routes */
 app.use("/auth", auth);
 
+/** Start server */
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
