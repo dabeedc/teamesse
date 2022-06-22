c447aa432da5fd886fe9092840c9167e8acdc2fd
Scott <scott.m.king@outlook.com>
2022-06-21 21:27:37 -0700

    Add stuff to sidebar

--- pomodoro/src/sidebar/Sidebar.js
+++ pomodoro/src/sidebar/Sidebar.js
@@ -1,93 +1,107 @@
 import React from "react";
-import {
-  Box,
-  Button,
-  TextField,
-  Avatar,
-} from "@mui/material";
+import { Box, Button, TextField, Avatar, Typography } from "@mui/material";
+import { useSelector } from "react-redux";
 
 export const Sidebar = () => {
+  const { currentUser } = useSelector((state) => state.account);
+
   return (
     <>
-		<Box
-			sx={{
-				display: "flex",
-				flexDirection: "column",
-				alignItems: 'center',
-				justifyContent: 'space-evenly',
-				width: "300px",
-				height: "1080px",
-				backgroundColor: (theme) => theme.palette.common.third,
-			}}
-		>
-			<Avatar
-				sx={{
-				backgroundColor: "common.second",
-				width: 150,
-				height: 150,
-				}}
-			/>
-			<Box
-				sx={{
-					display: "flex",
-					flexDirection: "column",
-					alignItems: 'center',
-					justifyContent: "space-evenly",
-					height: "200px"
-				}}	
-			>
-				<Button
-					variant="contained"
-					sx={{ backgroundColor: "common.fourth", width: "200px" }}
-					>
-					Pomodoro
-				</Button>
-				<Button
-					variant="contained"
-					sx={{ backgroundColor: "common.fourth", width: "200px" }}
-				>
-				Statistics
-				</Button>
-				<Button
-					variant="contained"
-					sx={{ backgroundColor: "common.fourth", width: "200px" }}
-				>
-				Explore
-				</Button>
-			</Box>
-			<TextField
-				sx={{ width: "70%", backgroundColor: "common.second" }}
-				multiline
-				rows={10}
-				label="Weekly Goals"
-				defaultValue=""
-				variant="filled"
-			/>
-			<Box
-				sx={{
-					display: "flex",
-					flexDirection: "row",
-					alignItems: 'center',
-					justifyContent: 'space-evenly',
-					width: "80%",
-					height: "80px",
-					borderRadius: "16px",
-					backgroundColor: (theme) => theme.palette.common.second,
-				}}	
-			>
-				<Button
-					variant="contained"
-					sx={{ backgroundColor: "common.fourth" }}
-				>
-					Login
-				</Button>
-				<Button
-					variant="contained"
-					sx={{ backgroundColor: "common.fourth" }}
-				>
-					Settings
-				</Button>
-			</Box>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "space-evenly",
+          width: "300px",
+          height: "100vh",
+          backgroundColor: (theme) => theme.palette.common.third,
+        }}
+      >
+        {currentUser && (
+          <div>
+            <Typography variant="h5">Welcome back,</Typography>
+            <Typography variant="h3" sx={{ mt: 2 }}>
+              {currentUser?.name}
+            </Typography>
+          </div>
+        )}
+        <Avatar
+          sx={{
+            backgroundColor: "common.second",
+            width: "70%",
+            height: "auto",
+          }}
+          src={currentUser?.avatar}
+        />
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "space-evenly",
+            height: "200px",
+          }}
+        >
+          <Button
+            variant="contained"
+            sx={{ backgroundColor: "common.fourth", width: "200px" }}
+          >
+            Pomodoro
+          </Button>
+          <Button
+            variant="contained"
+            sx={{ backgroundColor: "common.fourth", width: "200px" }}
+          >
+            Statistics
+          </Button>
+          <Button
+            variant="contained"
+            sx={{ backgroundColor: "common.fourth", width: "200px" }}
+          >
+            Explore
+          </Button>
+        </Box>
+        <TextField
+          sx={{ width: "70%", backgroundColor: "common.second" }}
+          multiline
+          rows={10}
+          label="Weekly Goals"
+          defaultValue=""
+          variant="filled"
+        />
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-evenly",
+            width: "80%",
+            height: "80px",
+            borderRadius: "16px",
+            backgroundColor: (theme) => theme.palette.common.second,
+          }}
+        >
+          {currentUser ? (
+            <Button
+              variant="contained"
+              sx={{ backgroundColor: "common.fourth" }}
+            >
+              Logout
+            </Button>
+          ) : (
+            <Button
+              variant="contained"
+              sx={{ backgroundColor: "common.fourth" }}
+            >
+              Login
+            </Button>
+          )}
+
+          <Button variant="contained" sx={{ backgroundColor: "common.fourth" }}>
+            Settings
+          </Button>
+        </Box>
       </Box>
     </>
   );
