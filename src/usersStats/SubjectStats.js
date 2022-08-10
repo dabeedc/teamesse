c65c09a9dd737a0d248181c5dbf014d9e33e6fb9
Scott <scott.m.king@outlook.com>
2022-08-10 11:54:03 -0700

    Fix no data issue

--- src/usersStats/SubjectStats.js
+++ src/usersStats/SubjectStats.js
@@ -12,8 +12,9 @@ import { StatsToolTip } from "./StatsToolTip";
 
 export const SubjectStats = () => {
   const { currentUser } = useSelector((state) => state.account);
-  const [subjectData, setSubjectData] = useState([]);
+  const [subjectData, setSubjectData] = useState(null);
   const theme = useTheme();
+
   useEffect(() => {
     (async () => {
       let subjectRes = await fetch(
@@ -45,28 +46,30 @@ export const SubjectStats = () => {
         backgroundColor: () => theme.palette.common.sixth,
       }}
     >
-      <ResponsivePie
-        margin={{ top: 50, right: 75, bottom: 85, left: 75 }}
-        cornerRadius={3}
-        borderWidth={1}
-        arcLabelsTextColor={theme?.palette?.text?.secondary}
-        theme={{
-          textColor: theme?.palette?.text?.primary,
-          fontSize: 20,
-          tooltip: { container: { color: "black" } },
-        }}
-        data={subjectData.length > 0 ? subjectData : initialData}
-        tooltip={({ datum }) => {
-          const { color, data } = datum;
-          return (
-            <StatsToolTip color={color}>
-              <Typography sx={{ mr: "3px" }}>{data?.label}:</Typography>
-              <Typography fontWeight="bold">{data?.value} minutes</Typography>
-            </StatsToolTip>
-          );
-        }}
-        arcLinkLabelsTextColor={theme?.palette?.text?.primary}
-      />
+      {subjectData && (
+        <ResponsivePie
+          margin={{ top: 50, right: 75, bottom: 85, left: 75 }}
+          cornerRadius={3}
+          borderWidth={1}
+          arcLabelsTextColor={theme?.palette?.text?.secondary}
+          theme={{
+            textColor: theme?.palette?.text?.primary,
+            fontSize: 20,
+            tooltip: { container: { color: "black" } },
+          }}
+          data={subjectData.length > 0 ? subjectData : initialData}
+          tooltip={({ datum }) => {
+            const { color, data } = datum;
+            return (
+              <StatsToolTip color={color}>
+                <Typography sx={{ mr: "3px" }}>{data?.label}:</Typography>
+                <Typography fontWeight="bold">{data?.value} minutes</Typography>
+              </StatsToolTip>
+            );
+          }}
+          arcLinkLabelsTextColor={theme?.palette?.text?.primary}
+        />
+      )}
     </Box>
   );
 };
